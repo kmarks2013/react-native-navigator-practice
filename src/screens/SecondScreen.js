7a1e9068e5408a101b1423c08eddee8be75e50df
kmarks2013 <kmarks2013@gmail.com>
2020-04-12 14:09:34 -0400

    imports and initalizes stylesheet

--- src/screens/SecondScreen.js
+++ src/screens/SecondScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const SecondScreen = () => {
     return (
@@ -8,5 +8,9 @@ const SecondScreen = () => {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+
+})
 
 export default SecondScreen
