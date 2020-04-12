64b84286806fd4af507137db74cda0bfe42d9473
kmarks2013 <kmarks2013@gmail.com>
2020-04-12 14:40:20 -0400

    adds inital route

--- App.js
+++ App.js
@@ -3,4 +3,9 @@ import HomeScreen from './src/screens/HomeScreen'
 
 const navigator = createStackNavigator({
   Home: HomeScreen
+}, {
+  initalRouteName: "Search",
+  defaultNavigationOptions: {
+    title: "App"
+  }
 })
