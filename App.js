f22af9fb0259235b5f4a3b64f39ff699e430e5d0
kmarks2013 <kmarks2013@gmail.com>
2020-04-12 14:27:27 -0400

    deletes starter code and imports navigation functions and creates navigatior with homescreen key value

--- App.js
+++ App.js
@@ -1,19 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigator'
+import HomeScreen from './src/screens/HomeScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const navigator = createStackNavigator({
+  Home: HomeScreen
+})
