eb9d03407d5239b0d61bebe1fbb7b4c2d9199884
kmarks2013 <kmarks2013@gmail.com>
2020-04-12 14:05:40 -0400

    adds src and screens folder and FirstScreen.js file with broiler plate text.

---
+++ src/screens/FristScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const FristScreen = () => {
+    return (
+        <View>
+            <Text>Hello From the first screen</Text>
+        </View>
+    )
+}
+
+const  styles = StyleSheet.create({
+
+})
+
+export default FristScreen
